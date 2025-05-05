95c71f5a083354593c4742810b2e80c639c2b601
turanlz <turanhasan013@gmail.com>
2025-05-06 03:37:24 +0400

    commit first

---
+++ wwwroot/js/script.js
@@ -0,0 +1,17 @@
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 250) {
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Hamar animasiya
+  });
+});
+
